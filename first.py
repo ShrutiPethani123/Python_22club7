c8fede04d0dbff1e74f9e1d29fd333ae34c4ffde
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2022-12-28 15:00:36 +0530

    'init

---
+++ first.py
@@ -0,0 +1,105 @@
+print("Hello",end=" ")
+print('Good Morning')
+
+"""
+multi line comment
+"""
+
+'''
+multi
+line
+comment
+
+'''
+
+# single line comment
+
+#  java is the best'est Language
+#  java is the best"est Language
+
+print("java is the best'est Language")
+print('java is the bes"est Languag')
+print('java is the bes\' est Languag')
+print('java is the best\test Languag')
+print('java is the bes\nest Languag')
+print('java is the bet\\\est Languag')
+print("royal",'technosoft')
+print("royal Technosoft")
+
+a=34
+print(a)
+print(type(a))
+
+a=-346946464684534684654168745341689436549874685
+print(a)
+print(type(a))
+
+a=34.6849845315684984685489746
+print(a)
+print(type(a))
+
+a='python'
+print(a)
+print(type(a))
+
+a='f'
+print(a)
+print(type(a))
+
+a=4+3j
+print(a)
+print(type(a))
+
+"""
+complex=> real + img
+
+real - 4 
+img - 3
+
+j^2 = -1
+
+"""
+
+a=True
+print(a)
+print(type(a))
+
+
+a=False
+print(a)
+print(type(a))
+
+"""
+
+int
+float
+str
+bool
+complex
+
+"""
+
+# a=input("Enter a no: ")
+# print(a)
+# print(type(a))
+
+"""
+Typecasting
+
+int()
+str()
+float()
+
+"""
+# a=int(input("Enter a no: "))
+# print(a)
+# print(type(a))
+
+# a=float(input("Enter a no: "))
+# print(a)
+# print(type(a))
+
+a=int(input("Enter a no: "))
+b=int(input("Enter a no:"))
+c=a+b
+print("Addition is",c)
